d4722308ee8264d475698df71f7a040fa3d4c96f
wellington <umcorte@gmail.com>
2020-09-01 18:22:36 -0300

    Implementando estoque

--- estoque/views.py
+++ estoque/views.py
@@ -7,3 +7,10 @@ def estoque_entrada_list(request):
     objects = Estoque.objects.filter(movimento='e')
     context = {'object_list': objects}
     return render(request, template_name, context)
+
+
+def estoque_entrada_detail(request, pk):
+    template_name = 'estoque_entrada_detail.html'
+    object = Estoque.objects.get(pk=pk)
+    context = {'object': object}
+    return render(request, template_name, context)
